bef92c40a7f5c6217e3b9014a25aa7431e01714d
James Thomas <jamesjacobthomas7@gmail.com>
2018-06-01 04:41:09 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,40 @@
 # your code goes here
+require 'pry'
+def begins_with_r(tools)
+  tools.all? do |tool|
+    tool.start_with?('r')
+  end 
+end
+    
+ def contain_a(array)
+   letter_a = []
+   array.each do |element|
+     if element.include? "a"
+       letter_a << element
+     end     
+  end
+  letter_a
+end
+
+def first_wa(array)
+  array.each do |element|
+    if element.is_a? String
+      if element.start_with?('wa')
+        return element
+      end        
+    end   
+  end
+end
+
+def remove_non_strings(array)
+array.delete_if { |obj| !(obj.is_a? String) }
+end
+
+def count_elements(array)
+  counter = Hash.new 0
+  array.each do |element|
+    counter[element]
+  binding.pry
+  end
+
+end
