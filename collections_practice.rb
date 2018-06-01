f0fec9618d7dbb995c6fd435e783b3aab381086f
James Thomas <jamesjacobthomas7@gmail.com>
2018-06-01 04:56:14 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -33,7 +33,7 @@ end
 def count_elements(array)
   counter = Hash.new 0
   array.each do |element|
-    counter[name] = element[name]
+    counter[:name] = element[:name]
     binding.pry
   end
   counter
